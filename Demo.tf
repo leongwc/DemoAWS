1d7393483117c0dfaa698b6fb260192a5628c079
leongwc <100122172+leongwc@users.noreply.github.com>
2022-06-30 21:34:26 +0800

    Update Demo.tf

--- Demo.tf
+++ Demo.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = "10.0.0.0/16"
+    cidr_blocks      = ["10.0.0.0/16"]
   }
 
   egress {
